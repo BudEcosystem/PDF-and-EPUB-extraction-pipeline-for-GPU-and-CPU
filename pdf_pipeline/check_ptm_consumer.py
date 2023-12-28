4c9f0d5fd9254ac751d6bc931b9d391b91708289
sonalidevbud <sonali.gupta@devbud.ai>
2023-12-28 14:57:39 +0000

    pdfigcapx removed

--- pdf_pipeline/check_ptm_consumer.py
+++ pdf_pipeline/check_ptm_consumer.py
@@ -3,6 +3,7 @@
 import os
 import traceback
 import sys
+
 sys.path.append("pdf_extraction_pipeline/code")
 sys.path.append("pdf_extraction_pipeline")
 import json
@@ -19,29 +20,24 @@ nougat_batch_size = int(os.environ["NOUGAT_BATCH_SIZE"])
 connection = get_rabbitmq_connection()
 channel = get_channel(connection)
 
-book_details = get_mongo_collection('book_details')
-figure_caption = get_mongo_collection('figure_caption')
-publaynet_pages = get_mongo_collection('publaynet_pages')
-table_bank_pages = get_mongo_collection('table_bank_pages')
-mfd_pages = get_mongo_collection('mfd_pages')
+book_details = get_mongo_collection("book_details")
+figure_caption = get_mongo_collection("figure_caption")
+publaynet_pages = get_mongo_collection("publaynet_pages")
+table_bank_pages = get_mongo_collection("table_bank_pages")
+mfd_pages = get_mongo_collection("mfd_pages")
 
-def get_fig_data(bookId, book_path):
-    fig_done = False
-    fig = figure_caption.find_one({
-        "bookId": bookId,
-        "split_path": book_path
-    })
-    if fig:
-        fig_done = True
-    return fig_done, fig
+
+# def get_fig_data(bookId, book_path):
+#     fig_done = False
+#     fig = figure_caption.find_one({"bookId": bookId, "split_path": book_path})
+#     if fig:
+#         fig_done = True
+#     return fig_done, fig
 
 
 def get_ptm_data(layout_name, bookId, page_num):
     layout_done = False
-    find_data = {
-        "bookId": bookId,
-        "pages.page_num": page_num
-    }
+    find_data = {"bookId": bookId, "pages.page_num": page_num}
     data = None
     if layout_name == "publaynet":
         data = publaynet_pages.find_one(find_data)
@@ -51,11 +47,11 @@ def get_ptm_data(layout_name, bookId, page_num):
         data = mfd_pages.find_one(find_data)
     if data:
         layout_done = True
-        data = list(filter(lambda x: x['page_num'] == page_num, data['pages']))
+        data = list(filter(lambda x: x["page_num"] == page_num, data["pages"]))
         # no check because layout block for page_num should always be present
         data = data[0]
     return layout_done, data
-    
+
 
 def check_ptm_status(ch, method, properties, body):
     print("hello pmt called")
@@ -64,31 +60,23 @@ def check_ptm_status(ch, method, properties, body):
     book_path = message["split_path"]
     page_num = message["page_num"]  # can be None if request comes from pdfigcapx
     try:
-        pdfigcapx_done, pdfigcapx_data = get_fig_data(bookId, book_path)
-        if pdfigcapx_done:
-            from_page = pdfigcapx_data["from_page"]
-            to_page = pdfigcapx_data["to_page"]
-            if page_num:
-                from_page = page_num
-                to_page = page_num
-            for page_no in range(from_page, to_page + 1):
-                # fig_result = list(filter(lambda x: x["page_num"] == page_no, pdfigcapx_data['pages']))
-                # without image_path
-                fig_result = pdfigcapx_data['pages'].get(str(page_no), [])
-                page_data = check_ptm(page_no, bookId)
-                if page_data:
-                    page_data["split_path"] = book_path
-                    process_page(page_data, fig_result)
+        page_data = check_ptm(page_num, bookId)
+        if page_data:
+            page_data["split_path"] = book_path
+            process_page(page_data)
         else:
-            print(f"figure extraction for {page_num} - {bookId} not yet completed")
+            print(
+                f"mfd, tablebank and publaynet extraction for {page_num} - {bookId} not yet completed"
+            )
     except Exception as e:
         print(traceback.format_exc())
         error = {
             "consumer": QUEUE_NAME,
             "consumer_message": message,
             "error": str(e),
-            "line_number": traceback.extract_tb(e.__traceback__)[-1].lineno} 
-        error_queue(book_path, bookId, error)      
+            "line_number": traceback.extract_tb(e.__traceback__)[-1].lineno,
+        }
+        error_queue(book_path, bookId, error)
     finally:
         print("message ack sent")
         ch.basic_ack(delivery_tag=method.delivery_tag)
@@ -100,10 +88,12 @@ def check_ptm(page_no, bookId):
     table_bank_done, table_bank_data = get_ptm_data("table_bank", bookId, page_no)
     mfd_done, mfd_data = get_ptm_data("mfd", bookId, page_no)
     if publaynet_done and table_bank_done and mfd_done:
-        publaynet_page_results = publaynet_data['result']
-        table_bank_page_results = table_bank_data['result']
-        mfd_page_results = mfd_data['result']
-        page_results = publaynet_page_results + table_bank_page_results + mfd_page_results
+        publaynet_page_results = publaynet_data["result"]
+        table_bank_page_results = table_bank_data["result"]
+        mfd_page_results = mfd_data["result"]
+        page_results = (
+            publaynet_page_results + table_bank_page_results + mfd_page_results
+        )
         image_path = publaynet_data["image_path"]
         if not page_results:
             page_results = [{"image_path": image_path}]
@@ -114,7 +104,7 @@ def check_ptm(page_no, bookId):
             "page_num": page_no,
             "results": page_results,
             "image_path": image_path,
-            "bookId": bookId
+            "bookId": bookId,
         }
     return process_page_data
 
@@ -125,52 +115,56 @@ def process_page(process_page_data, fig_result):
     image_path = process_page_data["image_path"]
     bookId = process_page_data["bookId"]
 
-    is_figure_present = False
-    if fig_result:
-        is_figure_present = True
-        # if pdfigcapx find figure then remove figures identified by publaynet
-        # and add pdfigcapx results
-        results = [block for block in results if "type" in block and block['type'] != 'Figure']
-        for each in fig_result:
-            each['image_path'] = image_path
-        # results.extend(fig_result['result'])
-        results.extend(fig_result)
+    # is_figure_present = False
+    # if fig_result:
+    #     is_figure_present = True
+    #     # if pdfigcapx find figure then remove figures identified by publaynet
+    #     # and add pdfigcapx results
+    #     results = [
+    #         block for block in results if "type" in block and block["type"] != "Figure"
+    #     ]
+    #     for each in fig_result:
+    #         each["image_path"] = image_path
+    #     # results.extend(fig_result['result'])
+    #     results.extend(fig_result)
     book_data = book_details.find_one({"bookId": bookId})
     num_nougat_pages = book_data.get("num_nougat_pages", [])
     num_latex_pages = book_data.get("num_latex_pages", [])
     num_other_pages = book_data.get("num_other_pages", [])
-    if not results or  \
-        not any('x_1' in block and 'y_1' in block for block in results) or  \
-        not any(block['type'] in ["Table", "Figure"] for block in results):
+    if (
+        not results
+        or not any("x_1" in block and "y_1" in block for block in results)
+        or not any(block["type"] in ["Table", "Figure"] for block in results)
+    ):
         nougat_queue_msg = {
             "image_path": image_path,
             "page_num": page_num,
-            "bookId": bookId
+            "bookId": bookId,
         }
         if page_num not in num_nougat_pages:
             book_details.find_one_and_update(
                 {"bookId": bookId},
                 {
-                    "$addToSet": {"nougat_pages": nougat_queue_msg, "num_nougat_pages": page_num}
-                }
+                    "$addToSet": {
+                        "nougat_pages": nougat_queue_msg,
+                        "num_nougat_pages": page_num,
+                    }
+                },
             )
             split_id = calculate_split_id(nougat_queue_msg)
             nougat_queue_msg["split_id"] = split_id
             send_to_queue("nougat_queue", nougat_queue_msg)
-    elif any(block['type'] == "Equation" for block in results):
+    elif any(block["type"] == "Equation" for block in results):
         latex_ocr_queue_msg = {
             "results": results,
             "image_path": image_path,
             "bookId": bookId,
             "page_num": page_num,
-            "is_figure_present": is_figure_present
+            # "is_figure_present": is_figure_present,
         }
         if page_num not in num_latex_pages:
             book_details.find_one_and_update(
-                {"bookId": bookId},
-                {
-                    "$addToSet": {"num_latex_pages": page_num}
-                }
+                {"bookId": bookId}, {"$addToSet": {"num_latex_pages": page_num}}
             )
             send_to_queue("latex_ocr_queue", latex_ocr_queue_msg)
     else:
@@ -179,28 +173,28 @@ def process_page(process_page_data, fig_result):
             "image_path": image_path,
             "bookId": bookId,
             "page_num": page_num,
-            "is_figure_present": is_figure_present,
+            # "is_figure_present": is_figure_present,
         }
         if page_num not in num_other_pages:
             book_details.find_one_and_update(
-                {"bookId": bookId},
-                {
-                    "$addToSet": {"num_other_pages": page_num}
-                }
+                {"bookId": bookId}, {"$addToSet": {"num_other_pages": page_num}}
             )
-            send_to_queue('other_pages_queue', other_pages_queue_msg)
+            send_to_queue("other_pages_queue", other_pages_queue_msg)
     book_data = book_details.find_one({"bookId": bookId})
     total_pages_in_book = book_data["num_pages"]
-    num_nougat_pages = len(book_data.get('num_nougat_pages', []))
-    num_latex_pages = len(book_data.get('num_latex_pages', []))
-    num_other_pages = len(book_data.get('num_other_pages', []))
-    if num_nougat_pages > 0 and num_nougat_pages + num_latex_pages + num_other_pages == total_pages_in_book:
+    num_nougat_pages = len(book_data.get("num_nougat_pages", []))
+    num_latex_pages = len(book_data.get("num_latex_pages", []))
+    num_other_pages = len(book_data.get("num_other_pages", []))
+    if (
+        num_nougat_pages > 0
+        and num_nougat_pages + num_latex_pages + num_other_pages == total_pages_in_book
+    ):
         send_to_queue("nougat_queue", {"bookId": bookId})
 
 
 def calculate_split_id(nougat_page: dict) -> str:
-    bookId = nougat_page['bookId']
-    page_num = nougat_page['page_num']
+    bookId = nougat_page["bookId"]
+    page_num = nougat_page["page_num"]
     book = book_details.find_one({"bookId": bookId})
     nougat_splits = book.get("nougat_splits", {})  # {0: [], 1: [], ...}
     split_id = None
@@ -212,7 +206,7 @@ def calculate_split_id(nougat_page: dict) -> str:
         page_exists = False
         for s_id, pages in nougat_splits.items():
             for page in pages:
-                if page['page_num'] == page_num:
+                if page["page_num"] == page_num:
                     split_id = s_id
                     page_exists = True
                     break
@@ -229,11 +223,10 @@ def calculate_split_id(nougat_page: dict) -> str:
             split_id = next_split_id
             nougat_splits[str(next_split_id)] = [nougat_page]
     book_details.update_one(
-        {"bookId": bookId},
-        {"$set": {"nougat_splits": nougat_splits}}
+        {"bookId": bookId}, {"$set": {"nougat_splits": nougat_splits}}
     )
     return str(split_id)
-        
+
 
 def consume_ptm_completion_queue():
     channel.basic_qos(prefetch_count=1, global_qos=False)
@@ -244,13 +237,13 @@ def consume_ptm_completion_queue():
     # Set up the callback function for handling messages from the queue
     channel.basic_consume(queue=QUEUE_NAME, on_message_callback=check_ptm_status)
 
-    print(f' [*] Waiting for messages on {QUEUE_NAME}. To exit, press CTRL+C')
+    print(f" [*] Waiting for messages on {QUEUE_NAME}. To exit, press CTRL+C")
     channel.start_consuming()
-   
+
 
 if __name__ == "__main__":
     try:
-        consume_ptm_completion_queue()      
+        consume_ptm_completion_queue()
     except KeyboardInterrupt:
         pass
     finally:
--- pdf_pipeline/element_extraction_utils.py
+++ pdf_pipeline/element_extraction_utils.py
@@ -10,6 +10,7 @@ from pdf_producer import send_to_queue
 
 latex_model = LatexOCR()
 
+
 def get_fig_caption(block, image_path):
     caption = ""
     image_id = generate_unique_id()
@@ -17,7 +18,7 @@ def get_fig_caption(block, image_path):
     # extraction of text from cropped image using pytesseract
     image = Image.open(block_image_path)
     text = pytesseract.image_to_string(image)
-    text = re.sub(r'\s+', ' ', text).strip()
+    text = re.sub(r"\s+", " ", text).strip()
     pattern = r"(Fig\.|Figure)\s+\d+"
     match = re.search(pattern, text)
     if match:
@@ -29,7 +30,12 @@ def get_fig_caption(block, image_path):
 
 def process_table(table_block, image_path, bookId, page_num):
     output = None
-    x1, y1, x2, y2 = table_block['x_1'], table_block['y_1'], table_block['x_2'], table_block['y_2']
+    x1, y1, x2, y2 = (
+        table_block["x_1"],
+        table_block["y_1"],
+        table_block["x_2"],
+        table_block["y_2"],
+    )
     img = cv2.imread(image_path)
     y1 -= 70
     if y1 < 0:
@@ -41,19 +47,19 @@ def process_table(table_block, image_path, bookId, page_num):
     y2 += 20
     if y2 > img.shape[0]:
         y2 = img.shape[0]
-    cropped_image = img[int(y1):int(y2), int(x1):int(x2)] 
-    tableId = generate_unique_id() 
+    cropped_image = img[int(y1) : int(y2), int(x1) : int(x2)]
+    tableId = generate_unique_id()
     table_image_path = os.path.abspath(f"cropped_{tableId}.png")
     cv2.imwrite(table_image_path, cropped_image)
     output = f"{{{{table:{tableId}}}}}"
-    data = {'img': generate_image_str(bookId, table_image_path, save=False)}
+    data = {"img": generate_image_str(bookId, table_image_path, save=False)}
     bud_table_msg = {
-        'tableId': tableId,
-        'data': data,
-        'page_num': page_num,
-        'bookId': bookId
+        "tableId": tableId,
+        "data": data,
+        "page_num": page_num,
+        "bookId": bookId,
     }
-    send_to_queue('bud_table_extraction_queue', bud_table_msg)
+    send_to_queue("bud_table_extraction_queue", bud_table_msg)
     if os.path.exists(table_image_path):
         os.remove(table_image_path)
     return output
@@ -66,39 +72,24 @@ def process_figure(figure_block, image_path):
     output = f"{{{{figure:{figureId}}}}}"
 
     figure_url = upload_to_aws_s3(figure_image_path, figureId)
-    figure = {
-        "id": figureId,
-        "url": figure_url,
-        "caption": figure_block['caption']
-    }
+    figure = {"id": figureId, "url": figure_url, "caption": figure_block["caption"]}
     if os.path.exists(figure_image_path):
         os.remove(figure_image_path)
     return output, figure
 
 
-def process_publaynet_figure(figure_block, image_path, prev_block, next_block):
+def process_publaynet_figure(figure_block, image_path):
     print("publaynet figure")
     output = None
     caption = ""
     figureId = generate_unique_id()
     figure_image_path = crop_image(figure_block, image_path, figureId)
     output = f"{{{{figure:{figureId}}}}}"
-
-    if prev_block:
-        caption = get_fig_caption(prev_block, image_path)
-
-    if next_block and not caption:
-        caption = get_fig_caption(next_block, image_path)
-
     figure_url = upload_to_aws_s3(figure_image_path, figureId)
-    figure = {
-        "id": figureId,
-        "url": figure_url,
-        "caption": caption
-    }
+    figure = {"id": figureId, "url": figure_url, "caption": caption}
     if os.path.exists(figure_image_path):
         os.remove(figure_image_path)
-    return output, figure    
+    return output, figure
 
 
 def process_text(text_block, image_path):
@@ -110,8 +101,8 @@ def process_text(text_block, image_path):
     if os.path.exists(cropped_image_path):
         os.remove(cropped_image_path)
     return output
-    
-    
+
+
 def process_title(title_block, image_path):
     title_id = generate_unique_id()
     cropped_image_path = crop_image(title_block, image_path, title_id)
@@ -121,38 +112,37 @@ def process_title(title_block, image_path):
     if os.path.exists(cropped_image_path):
         os.remove(cropped_image_path)
     return output
-    
+
 
 def process_list(list_block, image_path):
     list_id = generate_unique_id()
     cropped_image_path = crop_image(list_block, image_path, list_id)
-    image =Image.open(cropped_image_path)
+    image = Image.open(cropped_image_path)
     text = pytesseract.image_to_string(image)
     output = text
     if os.path.exists(cropped_image_path):
         os.remove(cropped_image_path)
     return output
-    
+
 
 def process_equation(equation_block, image_path):
     equation_id = generate_unique_id()
     equation_image_path = crop_image(equation_block, image_path, equation_id)
     output = f"{{{{equation:{equation_id}}}}}"
     img = Image.open(equation_image_path)
-    latex_text = latex_model(img)
+    latex_text = ""
+    try:
+        latex_text = latex_model(img)
+    except Exception as e:
+        print("error while extracting equation through latex ocr", e)
     text_to_speech = latext_to_text_to_speech(latex_text)
-    equation = {
-        'id': equation_id,
-        'text': latex_text,
-        'text_to_speech': text_to_speech
-    }
+    equation = {"id": equation_id, "text": latex_text, "text_to_speech": text_to_speech}
     if os.path.exists(equation_image_path):
         os.remove(equation_image_path)
     return output, equation
 
 
 def latext_to_text_to_speech(text):
-    text = "${}$".format(text.lstrip('\\'))
+    text = "${}$".format(text.lstrip("\\"))
     text_to_speech = latex_to_text(text)
     return text_to_speech
-
